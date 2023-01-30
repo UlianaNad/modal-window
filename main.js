9c50e0927b79fcd5e91b31b6c5570450201fd42b
Uliana <unadorozhna1@gmail.com>
2023-01-30 13:11:51 +0200

    select gender added

--- main.js
+++ main.js
@@ -1,3 +1,4 @@
+// modal windows
 const openButtons = document.querySelectorAll("[data-modal-id]");
 const closeButtons = document.querySelectorAll("[data-button-id]")
 
@@ -16,4 +17,25 @@ closeButtons.forEach(button => {
         const modal = document.getElementById(modalId);
         modal.classList.remove("show");
     })
+})
+
+// select gender
+const gender = document.querySelector(".gender");
+gender.addEventListener('change', () => {
+    let genderValue = gender.value;
+    console.log(genderValue);
+
+    const alert = document.querySelector(".alert");
+    if(genderValue === "female"){
+        html = `It is not tolerant to ask woman's age!`;
+        alert.innerHTML = html;
+    } 
+    if(genderValue === "male"){
+        html2 = `<input type = "date">`;
+        alert.innerHTML = html2;
+    }
+    if(genderValue === "none"){
+        html = `Please, select your gender!`;
+        alert.innerHTML = html;
+    } 
 })
